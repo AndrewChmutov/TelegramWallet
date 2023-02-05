fa84fd8765effed7d5774758ff5679a6e30d1f82
Andrii Chmutov <ulman.andre@gmail.com>
2023-02-05 16:28:49 +0100

    Initializing bot logic
    Added basic commands, works well

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+import telegram
 import telegram.ext
+from telegram.ext import CallbackContext
 
 TOKEN = open('token.txt', mode='r').read()
 
+
+def start_command(update: telegram.Update, context: CallbackContext):
+    response = open('commands/start.txt', 'r').read()
+    update.message.reply_text(response, parse_mode=telegram.ParseMode.MARKDOWN)
+
+
+def help_command(update: telegram.Update, context: CallbackContext):
+    response = open('commands/help.txt', 'r').read()
+    update.message.reply_text(response)
+
+
+def error_handler(update: telegram.Update, context: CallbackContext):
+    print(f'Update: {update}, \n\nerror: {context.error}')
+
+
+def create_updater(token) -> telegram.ext.Updater:
+    updater = telegram.ext.Updater(token)
+    
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(telegram.ext.CommandHandler('start', start_command))
+    dispatcher.add_handler(telegram.ext.CommandHandler('help', help_command))
+
+    return updater
+
+
 if __name__ == '__main__':
-    print(TOKEN)
+    updater = create_updater(TOKEN)
+
+    updater.start_polling()
+    updater.idle()
