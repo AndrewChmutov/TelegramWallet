0a5d6ae84725531803dcbba9d16d68a82a272dbb
Andrii Chmutov <ulman.andre@gmail.com>
2023-02-06 00:30:11 +0100

    Main menu of the bot, raw

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import telegram
 import telegram.ext
+from telegram import ReplyKeyboardMarkup
+from telegram import KeyboardButton
 from telegram.ext import CallbackContext
 
 # custom library
@@ -24,6 +26,26 @@ def help_command(update: telegram.Update, context: CallbackContext):
     update.message.reply_text(response)
 
 
+# Main menu of actions
+def menu_command(update: telegram.Update, context: CallbackContext):
+    exrates = KeyboardButton('Exchange rates')
+    balance = KeyboardButton('Balance')
+    markup_reply = ReplyKeyboardMarkup([[exrates], [balance]], resize_keyboard=True)
+    context.bot.send_message(update.message.chat.id, 'Enter action:', reply_markup=markup_reply)
+
+    return 1
+
+
+# fallback
+def cancel():
+    pass
+
+
+# Handle the result of the choice in the main menu
+def menu_message_handler(update: telegram.Update, context: CallbackContext):
+    update.message.reply_text(update.message.text.upper())
+
+
 def error_handler(update: telegram.Update, context: CallbackContext):
     print(f'Update: {update}, \n\nerror: {context.error}')
 
@@ -35,6 +57,17 @@ def create_updater(token) -> telegram.ext.Updater:
     dispatcher = updater.dispatcher
     dispatcher.add_handler(telegram.ext.CommandHandler('start', start_command))
     dispatcher.add_handler(telegram.ext.CommandHandler('help', help_command))
+
+    # main conversation
+    conversation_handler = telegram.ext.ConversationHandler(
+        entry_points=[telegram.ext.CommandHandler('menu', menu_command)],
+        states = {
+            1: [telegram.ext.MessageHandler(telegram.ext.Filters.text, menu_message_handler)]
+        },
+        fallbacks=[telegram.ext.CommandHandler('cancel', cancel)]
+    )
+
+    dispatcher.add_handler(conversation_handler)
 
     return updater
 
