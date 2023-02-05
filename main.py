15d51d6a5d601746dc380eb7e78ecb96ac2325aa
Andrii Chmutov <ulman.andre@gmail.com>
2023-02-05 16:49:57 +0100

    Added some comments

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import telegram
 import telegram.ext
 from telegram.ext import CallbackContext
 
+# token of the bot. For individual use, you should enter yous
 TOKEN = open('token.txt', mode='r').read()
 
 
+# handling commands and conversations
 def start_command(update: telegram.Update, context: CallbackContext):
     response = open('commands/start.txt', 'r').read()
     update.message.reply_text(response, parse_mode=telegram.ParseMode.MARKDOWN)
@@ -19,6 +21,7 @@ def error_handler(update: telegram.Update, context: CallbackContext):
     print(f'Update: {update}, \n\nerror: {context.error}')
 
 
+# add functionality to the bot
 def create_updater(token) -> telegram.ext.Updater:
     updater = telegram.ext.Updater(token)
     
@@ -29,6 +32,7 @@ def create_updater(token) -> telegram.ext.Updater:
     return updater
 
 
+# main body
 if __name__ == '__main__':
     updater = create_updater(TOKEN)
 
